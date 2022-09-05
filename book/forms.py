c9ea1f2f520098acff2a07d3edee140b27267155
AnneKammenhed <anne.juriado@gmail.com>
2022-09-05 15:20:24 +0000

    add forms.py and view and try to get csrf token working in html

--- book/forms.py
+++ book/forms.py
@@ -1,13 +1,14 @@
 from django.db import models
-# from django import forms
+from django.forms import ModelForm
 # from django.contrib.auth.models import User
 # from cloudinary.models import CloudinaryField
+from .models import Booking
 
 
-# class BookingForm(forms.ModelForm):
-#    class Meta:
-#        model = Booking
-#        fields = '__all__'
+class BookingForm(ModelForm):
+    class Meta:
+        model = Booking
+        fields = '__all__'
 
 #    def__init__(self, *args, **kwargs):
 #        guest = kwargs.pop('guest', None)
--- book/views.py
+++ book/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
-# from django.views.generic.edit import FormView
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views.generic.edit import FormView
 # from django import forms
-from django.views import generic
+from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Booking
+from .forms import BookingForm
 
-# view to see the bookings tried FormView 
+
+# view to see the bookings
 class BookingView(generic.ListView):
     template_name = 'index.html'
     model = Booking
     queryset = Booking.objects.order_by("-booking_day")
-#    success_url = '/Thanks for booking!/'
+
+# view to see the bookings form FormView
+class BookingFormView(FormView):
+    template_name = 'index.html'
+    form_class = BookingForm
+    success_url = '/Thanks for booking!/'
+
 
 #    def form_valid(self, form):
-#        form.send_email()
-#        return super().form_valid(form)
+#        booking = booking.objects.create_booking(
+#            booking_day=form.cleaned_data['booking_day']
+#        )
+#
+#        return super(BookingFormView, self).form_valid(form)

23fab318c7ded05209267796936c77525b12d558
AnneKammenhed <anne.juriado@gmail.com>
2022-09-06 06:46:28 +0000

    add fields and labels to forms.py

--- book/forms.py
+++ book/forms.py
@@ -8,13 +8,26 @@ class BookingForm(ModelForm):
 
     class Meta:
         model = Booking
-        fields = ['booking_day']
-        labels = {'booking_day': 'Booking day'}
+        fields = [
+            'booking_day', 
+            'booking_time', 
+            'number_of_guests', 
+            'guest_email',
+            'allergies',
+        ]
+        
+        labels = {
+            'booking_day': 'Booking day',
+            'booking_time': 'Booking time',
+            'number_of_guests': 'Max 8 guests online',
+            'guest_email': 'Your email address',
+            'allergies': 'Any allergies?'
+        }
 
 
 #        fields = '__all__'
 
 
 # forms.Form
-    guest = forms.CharField(max_length=100)
-    email = forms.EmailField()
+#    guest = forms.CharField(max_length=100)
+#    email = forms.EmailField()

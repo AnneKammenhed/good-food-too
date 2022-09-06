0c9c7878e1e381a789bd8d397cc44607bc296178
AnneKammenhed <anne.juriado@gmail.com>
2022-09-06 05:44:07 +0000

    add connection between forms.py and index.html

--- book/forms.py
+++ book/forms.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django import forms
 from .models import Booking
+from django.forms import ModelForm
 
 
-class BookingForm(forms.ModelForm):
+class BookingForm(ModelForm):
+
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = ['booking_day']
+        labels = {'booking_day': 'Booking day'}
+
+
+#        fields = '__all__'
+
+
+# forms.Form
+    guest = forms.CharField(max_length=100)
+    email = forms.EmailField()
--- book/views.py
+++ book/views.py
@@ -1,24 +1,16 @@
-# from django.shortcuts import render #, get_object_or_404, reverse
-
 from django.forms import ModelForm
 from django.views.generic.edit import FormView
 
 from django.views import generic, View
-# from django.http import HttpResponseRedirect
-# from django.db import models
 
-# from django.db import models
+from django.db import models
 from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
-from .models import Booking
 from .forms import BookingForm
 
-
-# view to see the bookings
-class BookingView(generic.ListView):
-    template_name = 'index.html'
-    model = Booking
-    queryset = Booking.objects.order_by("-booking_day")
 
 # view to see the bookings form FormView
 class BookingFormView(FormView):
@@ -26,33 +18,12 @@ class BookingFormView(FormView):
     form_class = BookingForm
     success_url = 'menu'
 
-    def form (self, request):
-
-        return render (
-            request,
-            'index.html',
-            {
-            "booking_form": BookingForm()
-            },
-        )
-
-#        if request.method == 'POST':
-#            form = BookingForm(request.POST)
-#            if form.is_valid():
-#                form_data = form.cleaned_data
-#                message = f'Thank you {form_data[guest]}'
-#        else:
-#            form = BookingForm()
-#        
-#        return render(request, 'index.html', {"book": form})
-
-#    def post(self, request, *args, **kwargs):
-
-#        post = get_object_or_404(Booking, guest=guest)
-#        (data=request.POST)
-
-#        booking = booking.objects.create_booking(
-#            booking_day=form.cleaned_data['booking_day']
-#        )
-#
-#        return super(BookingFormView, self).form_valid(form)
+    def book(request):
+        if request.method == 'POST':
+            form = BookingForm(request.POST)
+            if form.is_valid():
+                return HttpResponseRedirect(reverse('menu'))
+        else:
+            form = BookingForm()
+        
+        return render(request, 'index.html', {"form": form})
